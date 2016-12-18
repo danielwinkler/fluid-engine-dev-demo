ab22081bd736da9d0aba8a57a5dc0d0809a68999
Doyub Kim <doyubkim@gmail.com>
2016-12-18 07:47:51 +0000

    Add more scripts

---
+++ scripts/smoke_sim_demo04.sh
@@ -0,0 +1,2 @@
+NAME=smoke_sim_demo04
+bin/smoke_sim -e 4 -r 200 -f 300 -o $NAME -l $NAME.log -m vol
---
+++ scripts/smoke_sim_demo05.sh
@@ -0,0 +1,2 @@
+NAME=smoke_sim_demo05
+bin/smoke_sim -e 5 -r 200 -f 300 -o $NAME -l $NAME.log -m vol
